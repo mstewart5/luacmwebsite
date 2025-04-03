4a891a9da0a35f29216800bfeb9ec7e2aaefead4
mstewart5 <binaryx7@gmail.com>
2025-04-02 20:46:16 -0500

    Update website favicon

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -2,15 +2,13 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans, IBM_Plex_Mono } from "next/font/google";
 import "./globals.css";
 
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 
 // If loading a variable font, you don't need to specify the font weight
 const plexSans = IBM_Plex_Sans({
   weight: "400",
-  subsets: ["latin"]
+  subsets: ["latin"],
 });
 
 const plexMono = IBM_Plex_Mono({
@@ -30,12 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body
         className={`${plexSans.className} ${plexMono.className} antialiased`}
       >
-
-
-
         <Navbar />
 
         {children}
